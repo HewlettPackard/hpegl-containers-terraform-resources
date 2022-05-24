aaf24a4756d0dcb43bf57d168e14bd1ea73f23af
kratigupta99 <krati.gupta@hpe.com>
2022-05-24 13:46:37 +0530

    Data source for mbp created

--- pkg/resources/registration.go
+++ pkg/resources/registration.go
@@ -25,6 +25,7 @@ func (r Registration) SupportedDataSources() map[string]*schema.Resource {
 	return map[string]*schema.Resource{
 		"hpegl_caas_cluster_blueprint": resources.DataSourceClusterBlueprint(),
 		"hpegl_caas_site":              resources.DataSourceAppliance(),
+		"hpegl_caas_machine_blueprint" : resources.DataSourceMachineBlueprint(),
 	}
 }
 
@@ -32,6 +33,7 @@ func (r Registration) SupportedResources() map[string]*schema.Resource {
 	return map[string]*schema.Resource{
 		"hpegl_caas_cluster_blueprint": resources.ClusterBlueprint(),
 		"hpegl_caas_cluster":           resources.Cluster(),
+		"hpegl_caas_machine_blueprint": resources.MachineBlueprint(),
 	}
 }
 
