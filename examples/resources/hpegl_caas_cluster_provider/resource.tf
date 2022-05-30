632875b316a7fd10755f52042be9907805bd48ce
agartanu-git <tanushi.agarwal@hpe.com>
2022-05-30 15:53:12 +0530

    Datasource for cluster provider

--- examples/resources/hpegl_caas_cluster_provider/resource.tf
+++ examples/resources/hpegl_caas_cluster_provider/resource.tf
@@ -24,34 +24,12 @@ data "hpegl_caas_site" "blr" {
   space_id = "aadc51f2-8b3f-4ae0-aff2-820f7169447f"
 }
 
-data "hpegl_caas_machine_blueprint" "mbcontrolplane" {
-  name = "standard-master"
-  site_id = data.hpegl_caas_site.blr.id
-}
-
-data "hpegl_caas_machine_blueprint" "mbworker" {
-  name = "standard-worker"
-  site_id = data.hpegl_caas_site.blr.id
-}
-
 data "hpegl_caas_cluster_provider" "clusterprovider" {
   name = "ecp"
   site_id = data.hpegl_caas_site.blr.id
 }
 
-resource hpegl_caas_cluster_blueprint testbp {
-  name         = "tf-cluster-bp"
-  k8s_version  = data.hpegl_caas_cluster_provider.clusterprovider.k8s_versions[0]
-  default_storage_class = "gl-sbc-glhcnimblestor"
-  site_id = data.hpegl_caas_site.blr.id
-  cluster_provider = "ecp"
-  control_plane_nodes = {
-    machine_blueprint_id = data.hpegl_caas_machine_blueprint.mbcontrolplane.id
-    count = "1"
-  }
-  worker_nodes {
-      name = "worker-1"
-      machine_blueprint_id = data.hpegl_caas_machine_blueprint.mbcontrolplane.id
-      count = "1"
-    }
+output "cluster_provider" {
+  description = "Storage classes information"
+  value       = data.hpegl_caas_cluster_provider.clusterprovider.*
 }
