c2334088d219e61938d547c888b34b1ede7eca4b
kjrach <rachana.kallada-jayaraj@hpe.com>
2022-06-15 13:54:13 +0530

    Test

--- internal/acceptance_test/resource_caas_cluster_test.go
+++ internal/acceptance_test/resource_caas_cluster_test.go
@@ -113,7 +113,7 @@ func testCaasClusterDestroy(name string) resource.TestCheckFunc {
 			return err
 		}
 
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
 		defer cancel()
 
 		token, err := auth.GetToken(ctx, testAccProvider.Meta())
