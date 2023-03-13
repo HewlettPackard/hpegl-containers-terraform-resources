3686fec729792ce6e1310e4f572e6f0f2d38a1fb
shreyas-h-d <shreyas.h-d@hpe.com>
2023-03-13 23:52:36 +0530

    fix lint

--- internal/acceptance_test/resource_caas_cluster_test.go
+++ internal/acceptance_test/resource_caas_cluster_test.go
@@ -52,10 +52,10 @@ func testCaasCluster(clusterName string) string {
 		blueprint_id = data.hpegl_caas_cluster_blueprint.bp.id
         site_id = data.hpegl_caas_site.site.id
 		space_id     = var.HPEGL_SPACE
-	}
-	timeouts {
-        create = "2h"
-    }`, apiURL, siteName, clusterName)
+		timeouts {
+			create = "2h"
+		}
+	}`, apiURL, siteName, clusterName)
 }
 
 // nolint: gosec
