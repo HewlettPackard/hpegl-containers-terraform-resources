33dc2930aae8057f064284070ccaa868dc9a1e85
agartanu-git <tanushi.agarwal@hpe.com>
2023-03-17 17:30:15 +0530

    Add support for os image update

--- internal/resources/cluster_blueprint.go
+++ internal/resources/cluster_blueprint.go
@@ -53,7 +53,7 @@ func clusterBlueprintCreateContext(ctx context.Context, d *schema.ResourceData, 
 	for _, workerNode := range workerNodesList {
 		worker, ok := workerNode.(map[string]interface{})
 		if ok {
-			workerNodeDetails := getWorkerNodeDetails(worker)
+			workerNodeDetails := getWorkerNodeDetails(d, worker)
 			machineSetsList = append(machineSetsList, workerNodeDetails)
 		}
 	}
@@ -165,12 +165,23 @@ func clusterBlueprintDeleteContext(ctx context.Context, d *schema.ResourceData, 
 	return diags
 }
 
-func getWorkerNodeDetails(workerNode map[string]interface{}) mcaasapi.MachineSet {
+func getWorkerNodeDetails(d *schema.ResourceData, workerNode map[string]interface{}) mcaasapi.MachineSet {
 	osImage := ""
 	osVersion := ""
-	if workerNode["os_image"] != nil && workerNode["os_version"] != nil {
+	machines, ok := d.GetOk("machine_sets")
+	machinesets := machines.([]interface{})
+
+	if workerNode["os_image"].(string) != "" && workerNode["os_version"].(string) != "" {
 		osVersion = fmt.Sprintf("%v", workerNode["os_version"])
 		osImage = fmt.Sprintf("%v", workerNode["os_image"])
+	} else if ok{
+		for _, machinesetInt := range machinesets {
+			machineset := machinesetInt.(map[string]interface{})
+			if workerNode["name"] == machineset["name"] {
+				osVersion = fmt.Sprintf("%v", machineset["os_version"])
+				osImage = fmt.Sprintf("%v", machineset["os_image"])
+			}
+		}
 	}
 	wn := mcaasapi.MachineSet{
 		MachineBlueprintId: workerNode["machine_blueprint_id"].(string),
@@ -180,5 +191,4 @@ func getWorkerNodeDetails(workerNode map[string]interface{}) mcaasapi.MachineSet
 		OsVersion:          osVersion,
 	}
 	return wn
-
 }
